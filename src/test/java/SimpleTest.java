c28743f8948b87337a951592136632708abf9d55
rleszcz <leszczynski.radoslaw@gmail.com>
2021-04-15 01:18:47 +0200

    trying to enable multiple browsers execution

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,22 +1,22 @@
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
 
 public abstract class SimpleTest {
     protected WebDriver driver;
     protected HomePage homePage = null;
     protected SearchResultsPage searchResultsPage = null;
     protected ItemPage itemPage = null;
+    protected DriverManager driverManager = null;
+
+    @Parameters("browserName")
+
     @BeforeMethod
-    public void preparation() {
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-        ChromeOptions co = new ChromeOptions();
-        co.addArguments("--disable-geolocation");
-        co.addArguments("--incognito");
-        co.addArguments("--start-maximized");
-        driver = new ChromeDriver(co);
+    public void preparation(String browserName, ITestContext context){
+        driverManager = new DriverManager();
+        driver = driverManager.getDriver(browserName);
+        context.setAttribute("WebDriver", driver);
+
         homePage = new HomePage(driver);
         searchResultsPage = new SearchResultsPage(driver);
         itemPage = new ItemPage(driver);
